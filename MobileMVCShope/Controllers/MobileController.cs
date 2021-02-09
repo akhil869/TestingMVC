d739f7dba5ea009ba8de046eb127a6c891c436e3
Ganesan Perumalsamy <Ganesan.Perumalsamy@dell.com>
2021-02-09 13:08:13 +0530

    Added files

---
+++ MobileMVCShope/Controllers/MobileController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MobileMVCShope.Models;
+using System.Data.Entity;
+
+namespace MobileMVCShope.Controllers
+{
+    public class MobileController : Controller
+    {
+        MobileEntities mobileEntities = new MobileEntities();
+        // GET: Mobile
+        public ActionResult ShowMobiles()
+        {
+            var mobilelist = mobileEntities.tblMobiles.ToList();
+
+            return View(mobilelist);
+        }
+        public ActionResult InsertMobile()
+        {
+            return View();
+        }
+        public ActionResult EditMobile(int id)
+        {
+            var mobiledata = mobileEntities.tblMobiles.Find(id);
+            return View(mobiledata);
+        }
+        [HttpPost]
+        public ActionResult EditMobile(tblMobile mobiledata)
+        {
+            mobileEntities.Entry(mobiledata).State = EntityState.Modified;
+            mobileEntities.SaveChanges();
+            return RedirectToAction("ShowMobiles");
+        }
+        public ActionResult DeleteMobile()
+        {
+            return View();
+        }
+        public string ShowData()
+        {
+            return "Data";
+        }
+    }
+}
